07bfad5d59db22a0e2ae73f4c38d8f81fb9e5e70
Your Name <you@example.com>
2024-11-11 18:00:57 +0100

    Update Practica.java

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -15,7 +15,11 @@ public class Practica {
 		
 		// User 2 fer la resta dels elements de subArray
 		
-		
+		int subResult = subArray[0];
+		for (int i = 1; i < subArray.length; i++) {
+			subResult -= subArray[i];
+		}
+		System.out.println("Resta de los elementos de subArray: " + subResult);
 		
 		// User 3 fer la multiplicació dels elements de prodArray
 	}
